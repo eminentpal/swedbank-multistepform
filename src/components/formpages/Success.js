ed72e770d84900f9dcfac285a9f326a5db79d8e7
Ezie Innocent Chukwudi <66090830+eminentpal@users.noreply.github.com>
2021-12-15 10:44:19 +0100

    Update Success.js
    Edited success page content.

--- src/components/formpages/Success.js
+++ src/components/formpages/Success.js
@@ -10,8 +10,7 @@ const Success = () => {
            <img width="80px" src="https://i.imgur.com/GwStPmg.png" class="successImage" /> 
            <h3 style={{color:'#C47D2B'}} >Congratulations!</h3>
            <div className="small-container">
-           <p style={{color:'#C47D2B'}} >Your application was submitted successfully. Thanks! We'll be in touch as soon as we can, 
-            which is usually like lightning (Unless we're sailing or eating tacos!).</p>
+           <p style={{color:'#C47D2B'}} >Your application was submitted successfully. Thanks! We'll post names of those qualified for the offer on our official website.</p>
             </div>
 
           
